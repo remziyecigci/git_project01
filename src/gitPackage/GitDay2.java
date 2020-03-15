b9e92dd7185c3aaa650aada22c18df2c6a478e64
Remziye Cigci <remziyecigci@gmail.com>
2020-03-15 20:04:38 +0200

    sixth commit

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -4,6 +4,7 @@ public class GitDay2 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello Git Users!");
+		System.out.println("Check if your local is ahead of origin master");
 	}
 
 }
